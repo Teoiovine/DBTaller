25baff565765b58d558e7baeab341cb0e4f2ce14
Teoiovine <teoiovine@gmail.com>
2015-07-04 22:39:39 -0300

    Agrego insertarAlumnos()

--- funcionesDB.php
+++ funcionesDB.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ------------------------------------------------------------------------------------------------------------
---                      funcionesDB.PHP: Archivo contenedor de todos los querys                                   --
+--                      funcionesDB.PHP: Archivo contenedor de todos los querys                           --
 --                                En construcción                                                         --
 --                                 Man at work                                                            --
 ------------------------------------------------------------------------------------------------------------
@@ -15,20 +15,56 @@ try {
     print "¡Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-//Seleccionar todos los alumnos
+
+//Seleccionar todos los alumnos, o por curso
 function seleccionarAlumnos($db, $curso = 0){
-	//Corrobora si se selecciono algun curso o es para todos los alumnos
 	$query = "SELECT dniAlumno, apellido, nombre, curso FROM alumnos";
 	$orderby = " ORDER BY curso, apellido";
+	//Corrobora si se selecciono algun curso o es para todos los alumnos
 	if ($curso != 0){
 		//Concatena la sentencia de condición
 		$query .= " WHERE curso = '$curso'";
 	}
 	//Concatena el query final con el orderBy y ejecuta el query
-	//Pasa elresultado a un array y lo devuelve
+	//Pasa el resultado a un array y lo devuelve
 	return $db->query($query.$orderby)->fetchAll();
 }
+
+//Insertar alumnos en masa
+function insertarAlumnos($db,$alumno){
+	//NOTA: Falta testear
+	//Comenzamos el try
+	try{
+		//Iniciamos una transacción, este comando desactiva el autocommit();
+		$db->beginTransaction();
+		//Preparamos la consulta para cargarle los datos después
+		$query = $db->prepare("INSERT INTO alumnos (dniAlumno, nombre, apellido, curso)
+						VALUES :dni, :nombre, :apellido, :curso");
+		//Cargamos los datos a la consulta y la ejecutamos
+		$query->exec(array(':dni'=>$alumno[0]
+						   ':nombre'=>$alumno[1]
+						   ':apellido'=>$alumno[2]
+						   ':curso'=>$alumno[3]));
+		//Ya que no hubo errores, se implementa la consulta
+		$db->commit();
+	} catch (PDOException $e{
+		//Esto pasa si hubo cualquier tipo de error, desde que empieza el try
+		//Lo que hace es imprimir el error, y dar marcha atrás para que cualquier cagada no quede implementada
+		print "¡Error!: " . $e->getMessage() . "<br/>";
+		$db->rollBack();
+    	die();
+	}	
+}
+
+//TO DO: 
+//SELECT * FROM material WHERE idMaterial=$id
+//SELECT * FROM material
+//INSERT INTO material VALUES(NULL,'$archivo_tipo' , '$archivo_nombre', '$archivo_referencia', '$archivo_extension','$archivo_idmateria')
+//Estas son las consultas SQL de la parte de manejo de archivos, deberíamos mejorarlas e implementarlas...
+//--------------------------------
+//Consultas de la parte de inasistencias (Selects updates e inserts)
 function closeDB($db){
+	//Chau
 	$db = null;
 }
 ?>
